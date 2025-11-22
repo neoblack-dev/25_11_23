a8f88f703466f24368814350cb6f867a6312a10d
neoblack-dev <neoblackrussian@gmail.com>
2025-11-23 03:07:32 +0500

    yeah

---
+++ source/main.py
@@ -0,0 +1,9 @@
+import math
+import random
+print('Hello World!')
+my_list = ['one', 'two', 'three']
+print(random.choice(my_list))
+def circle_per(radius):
+    return 2 * math.pi * radius
+def circle_area(radius):
+    return math.pi * radius * radius
---
+++ tests/test_main.py
@@ -0,0 +1,19 @@
+import pytest
+import math
+from source.main import circle_per, circle_area
+
+@pytest.fixture()
+def before_results():
+    print('\nдо теста')
+    yield
+    print('\nпосле теста')
+
+
+def test_circle_per():
+    assert circle_per(5) == 2 * math.pi * 5
+
+def test_circle_area(before_results):
+    assert circle_area(5) == math.pi * 5 * 5
+
+def test_circle_per_zero():
+    assert circle_per(0) == 0
