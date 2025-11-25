3d601ab140ac5bc6c5efdaf08b0e5a559f303c5d
neoblack-dev <neoblackrussian@gmail.com>
2025-11-26 04:37:15 +0500

    added request test

--- tests/test_site.py
+++ tests/test_site.py
@@ -1,4 +1,7 @@
-from playwright.sync_api import Page, expect
+from dataclasses import dataclass
+from time import sleep
+import re
+from playwright.sync_api import Page, expect, Route
 
 def test_wiki(page: Page):
     page.goto('https://www.wikipedia.org/')
@@ -11,3 +14,18 @@ def test_wiki2(page:Page):
     page.get_by_role('link', name='Содержание').click()
     page.locator('#ca-talk').click()
     expect(page.locator('#firstHeading')).to_have_text('Обсуждение Википедии:Содержание')
+
+def test_request(page:Page):
+    def change_request(route: Route):
+        data = route.request.post_data
+        if data:
+            data = data.replace('User', 'Usher')
+        print(data)
+        route.continue_(post_data=data)
+
+    page.route(re.compile('profile/authenticate'), change_request)
+    page.goto('https://gymlog.ru/profile/login/')
+    page.locator('#email').fill('User412')
+    page.locator('#password').fill('k9L-hL')
+    page.get_by_role('button', name='Войти').click()
+    sleep(5)
