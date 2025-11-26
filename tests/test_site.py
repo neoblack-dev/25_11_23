8cde1274e0f19d1a3efbb8bfd3d1e3652131774c
neoblack-dev <neoblackrussian@gmail.com>
2025-11-26 20:40:53 +0500

    added response test

--- tests/test_site.py
+++ tests/test_site.py
@@ -1,12 +1,12 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 from time import sleep
 import re
 from playwright.sync_api import Page, expect, Route
 
-def test_wiki(page: Page):
-    page.goto('https://www.wikipedia.org/')
-    page.get_by_role('link', name='Русский').click()
-    expect(page.get_by_text('Добро пожаловать в Википедию,')).to_be_visible()
+# def test_wiki(page: Page):
+#     page.goto('https://www.wikipedia.org/')
+#     page.get_by_role('link', name='Русский').click()
+#     expect(page.get_by_text('Добро пожаловать в Википедию,')).to_be_visible()
 
 def test_wiki2(page:Page):
     page.goto('https://www.wikipedia.org/')
@@ -29,3 +29,17 @@ def test_request(page:Page):
     page.locator('#password').fill('k9L-hL')
     page.get_by_role('button', name='Войти').click()
     sleep(5)
+
+def test_response(page: Page):
+    def change_response(route: Route):
+        response = route.fetch()
+        data = response.text()
+        data = data.replace('User412','пирожок')
+        route.fulfill(response=response, body=data)
+    page.route(re.compile('profile/412'), change_response)
+    page.goto('https://gymlog.ru/profile/login/')
+    page.locator('#email').fill('User412')
+    page.locator('#password').fill('k9L-hL')
+    page.get_by_role('button', name='Войти').click()
+    page.get_by_role('link', name='Мой профиль').click()
+    sleep(7)
